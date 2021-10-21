c4e1f2f0f66641b6802d3fbc644fe9835ee761a4
Valmir Farias <valmir751@hotmail.com>
2021-10-21 09:30:28 -0300

    CRUD scaffolding

---
+++ LojaWebMvc/Data/LojaWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LojaWebMvc.Models;
+
+namespace LojaWebMvc.Data
+{
+    public class LojaWebMvcContext : DbContext
+    {
+        public LojaWebMvcContext (DbContextOptions<LojaWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LojaWebMvc.Models.Department> Department { get; set; }
+    }
+}
--- LojaWebMvc/Startup.cs
+++ LojaWebMvc/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LojaWebMvc.Data;
 
 namespace LojaWebMvc
 {
@@ -24,6 +26,9 @@ namespace LojaWebMvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<LojaWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("LojaWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
